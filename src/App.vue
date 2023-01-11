276ad1910e17b6da6b5768be436efdc3350e650d
Andrzej Gębala <agebala@neonet.pl>
2023-01-11 17:00:21 +0100

    create few basic funcionality on vue

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">sprawdzamy ref</button>
+  
+  <div v-if="showModal">
+    <Modal theme="on" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">omen</a>
+        <a href="#">omen2</a>
+      </template>
+      <h1>jestem tu nowy</h1>
+      <p>pokaż cycki</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template @click="toggleModalTwo"></template>
+      <h1>jestem tu drugi raz</h1>
+      <p>to jest drugi raz!!</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">pokaż modal</button>
+  <button @click="toggleModalTwo">pokaż drugi podal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "MOJA PIERWSZA STYLÓWKA VUE",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.toggle("active");
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.active {
+  background: red;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{scale: theme === 'on'}">
+           <slot></slot>
+           <div>
+            <slot name="links"></slot>
+           </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['theme'],
+
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: #fff;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .scale{
+        background: tomato;
+        color: #fff;
+    }
+</style>
